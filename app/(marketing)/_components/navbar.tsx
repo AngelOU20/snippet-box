84ee8950f57bd73ce6d3473ec8babe81292fdbde
Julio Ucharima <julio_ucharima@usmp.pe>
2024-08-09 19:31:11 -0500

    add navbar components

---
+++ app/(marketing)/_components/navbar.tsx
@@ -0,0 +1,22 @@
+import Link from 'next/link';
+import React from 'react';
+import { Button } from '@/components/ui/button';
+import { Logo } from '@/components/logo';
+
+export const NavbarMarketing = () => {
+  return (
+    <div className="fixed top-0 w-full h-16 px-4 border-b shadow-sm bg-white flex items-center">
+      <div className="md:max-w-screen-2xl mx-auto flex items-center w-full justify-between">
+        <Logo />
+        <div className="space-x-4 md:block md:w-auto flex items-center justify-between w-full">
+          <Button variant="outline" asChild>
+            <Link href="/sign-in">Sign in</Link>
+          </Button>
+          <Button asChild>
+            <Link href="/sign-up">Get for free</Link>
+          </Button>
+        </div>
+      </div>
+    </div>
+  );
+};
