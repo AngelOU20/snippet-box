406dbd2b471ce5a3d3e0556feabecc99010756f8
Julio Ucharima <julio_ucharima@usmp.pe>
2024-08-09 20:21:43 -0500

    add logo component

---
+++ components/logo/logo.tsx
@@ -0,0 +1,15 @@
+import Image from 'next/image';
+import Link from 'next/link';
+
+export const Logo = () => {
+  return (
+    <Link href="/">
+      <div className="hover:opacity-75 transition items-center gap-x-3 md:flex hidden">
+        <Image src="/logo.svg" alt="logo" height={45} width={45} />
+        <p className="text-xl font-bold text-neutral-600">
+          <span className="text-cyan-600">Snippet</span> box
+        </p>
+      </div>
+    </Link>
+  );
+};
