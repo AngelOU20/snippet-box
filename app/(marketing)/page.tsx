9697c0f04f81e95f667c8d3f23b69d8be8ebe720
Julio Ucharima <julio_ucharima@usmp.pe>
2024-08-09 19:30:42 -0500

    add landing page

---
+++ app/(marketing)/page.tsx
@@ -0,0 +1,15 @@
+export default function MarketingPage() {
+  return (
+    <section className="flex flex-col justify-center items-center outfit p-4">
+      <h1 className="text-2xl md:text-4xl font-bold mb-8 text-center">
+        Organize Your Code Snippets{' '}
+        <span className="text-cyan-600">Efficiently!</span>
+      </h1>
+      <p className="text-center text-base md:text-lg w-[550px] max-sm:w-full text-slate-500">
+        With our advanced tagging and search features, you can quickly find the
+        snippet you need, right when you need it. Spend less time searching for
+        code and more time writing it.
+      </p>
+    </section>
+  );
+}
