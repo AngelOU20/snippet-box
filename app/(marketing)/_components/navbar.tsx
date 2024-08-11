f310db53433cb554f7b071f3467486edde8ae8b9
Julio Ucharima <julio_ucharima@usmp.pe>
2024-08-10 19:44:28 -0500

    change clerk version to 4.29.12

--- app/(marketing)/_components/navbar.tsx
+++ app/(marketing)/_components/navbar.tsx
@@ -2,19 +2,30 @@ import Link from 'next/link';
 import React from 'react';
 import { Button } from '@/components/ui/button';
 import { Logo } from '@/components/logo';
+import { auth } from '@clerk/nextjs';
 
 export const NavbarMarketing = () => {
+  const { userId } = auth();
+
   return (
     <div className="fixed top-0 w-full h-16 px-4 border-b shadow-sm bg-white flex items-center">
       <div className="md:max-w-screen-2xl mx-auto flex items-center w-full justify-between">
         <Logo />
         <div className="space-x-4 md:block md:w-auto flex items-center justify-between w-full">
-          <Button variant="outline" asChild>
-            <Link href="/sign-in">Sign in</Link>
-          </Button>
-          <Button asChild>
-            <Link href="/sign-up">Get for free</Link>
-          </Button>
+          {userId ? (
+            <Button>
+              <Link href="/my-notes">Access to the app</Link>
+            </Button>
+          ) : (
+            <>
+              <Button variant="outline" asChild>
+                <Link href="/sign-in">Sign in</Link>
+              </Button>
+              <Button asChild>
+                <Link href="/sign-up">Get for free</Link>
+              </Button>
+            </>
+          )}
         </div>
       </div>
     </div>
