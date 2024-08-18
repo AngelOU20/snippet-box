f3c4bbea1646f8b46c2dafe84f601fe914abe53d
Julio Ucharima <julio_ucharima@usmp.pe>
2024-08-17 19:50:21 -0500

    add dark class

--- components/navbar/search-bar.tsx
+++ components/navbar/search-bar.tsx
@@ -7,7 +7,7 @@ const AddSnippetButton = () => {
   return (
     <Button
       size={'sm'}
-      className="absolute bg-cyan-600 hover:bg-cyan-600/90 top-0.5 right-0 select-none"
+      className="absolute bg-cyan-600 hover:bg-cyan-600/90 dark:text-white top-0.5 right-0 select-none"
     >
       <Plus className="w-4 h-4" strokeWidth={4} /> Snippet
     </Button>
@@ -17,13 +17,13 @@ const AddSnippetButton = () => {
 export const SearchBar = ({ className }: { className?: string }) => {
   return (
     <div
-      className={`relative pl-3 w-4/5 h-10 bg-slate-100 rounded-md flex items-center gap-2 ${className}`}
+      className={`relative pl-3 w-4/5 h-10 bg-slate-100 dark:bg-background rounded-md flex items-center gap-2 ${className}`}
     >
       <Search />
       <Input
         type="text"
         placeholder="Search a snippet..."
-        className="w-[70%] border-0 text-sm bg-slate-100 text-slate-500"
+        className="w-[70%] border-0 text-sm bg-slate-100 text-slate-500 dark:bg-background dark:text-white"
       />
       <AddSnippetButton />
     </div>
